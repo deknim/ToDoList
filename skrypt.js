819d19e362a5437b2dc89674c7c38395fe5b55f2
deknim <129577635+deknim@users.noreply.github.com>
2023-05-03 13:15:59 +0200

    Wszystkie funkcje
    - dodawanie zadań,
    - przekreślanie ukończonych,
    - możliwość cofnięcia przekreślenia,
    - usuwanie zadań

--- skrypt.js
+++ skrypt.js
@@ -1,36 +1,43 @@
-
-//pobiera elementy z pliku html i  przypisuje je do zmiennych (pozwala na zarządzanie elementami z pliku html w pliku js)
 let toDoAddButton = document.getElementById('todo-add');
 let toDoContainer =  document.getElementById('todo-container');
 let toDoInput = document.getElementById('todo-input');
 
-//dodaje funkcję addEventListeners do zadań, wprowadzonych jako parametr
-const addEventListeners = paragraph => {
-    paragraph.addEventListener('click', () => {
-        paragraph.style.textDecoration = "line-through";
-        localStorage.setItem('tasks', toDoContainer.innerHTML);
-    });
-  
-    paragraph.addEventListener('dblclick', () => {
-        toDoContainer.removeChild(paragraph);
-        localStorage.setItem('tasks', toDoContainer.innerHTML);
-    });
-};
-  
-if (localStorage.getItem('tasks')) { //sprawdza czy w lokalnej pamięci znajdują się już jakieś zadania
-    toDoContainer.innerHTML = localStorage.getItem('tasks'); //jeśli tak, to umieszcza je w kontenerze
-        let paragraph = toDoContainer.getElementsByTagName('p'); //zwraca wszystkie elementy o tagu p w kontenerze i umieszcza je w zmiennej paragraph
-        for (let i = 0; i < paragraph.length; i++) { //pętla dodająca do każdego paragrafu funkcję addEventListeners
-            addEventListeners(paragraph[i]);
-        }
-}
+    function addEventListeners(paragraph){
+        const paragraphstyle = 'task';
+        paragraph.classList.add(paragraphstyle);
+        paragraph.addEventListener('click', () => {
+            if(paragraph.style.textDecoration === "none"){
+                paragraph.style.textDecoration = "line-through";
+            }
+            else {
+                paragraph.style.textDecoration = "none";
+            }
+            localStorage.setItem('tasks', toDoContainer.innerHTML);
+        });
 
-//dodaje do przycisku funkcję umożliwiającą dodawanie nowych zadań
-toDoAddButton.addEventListener('click', () => {
-    let paragraph = document.createElement('p');
-    paragraph.innerText = toDoInput.value;
-    toDoContainer.appendChild(paragraph);
-    toDoInput.value = ""; //czyści pole wpisywania zadań
-    addEventListeners(paragraph);   //dodaje do paragrafu z zadaniem funkcję addEventListeners
-    localStorage.setItem('tasks', toDoContainer.innerHTML);
-  });
+    let deleteButton = paragraph.querySelector('button') || document.createElement('button');
+        deleteButton.innerText = '-';
+        deleteButton.addEventListener('click', () => {
+            toDoContainer.removeChild(paragraph);
+            localStorage.setItem('tasks', toDoContainer.innerHTML);
+    });
+    paragraph.appendChild(deleteButton);
+    }
+  
+    if (localStorage.getItem('tasks')) {
+        toDoContainer.innerHTML = localStorage.getItem('tasks');
+            let paragraph = toDoContainer.getElementsByTagName('p');
+            for (let i = 0; i < paragraph.length; i++) {
+                addEventListeners(paragraph[i]);
+            }
+    }
+
+    toDoAddButton.addEventListener('click', () => {
+        let paragraph = document.createElement('p');
+            paragraph.innerText = toDoInput.value;
+            paragraph.style.textDecoration = "none";
+            toDoContainer.appendChild(paragraph);
+            toDoInput.value = "";
+            addEventListeners(paragraph);
+            localStorage.setItem('tasks', toDoContainer.innerHTML);
+    });
